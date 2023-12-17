a908d50ef5cc2d020a023dcf17aec37a0531f269
Huynh Phan <huynhpvhe170150@fpt.edu.vn>
2023-12-18 01:20:58 +0700

    Lấy ra danh sách bài viết

--- resolvers.ts
+++ resolvers.ts
@@ -1,7 +1,16 @@
+import Article from "./model/article.model";
+
 export const resolvers = {
-    Query: {
-      hello: () => {
-        return "Hello World";
-      },
+  Query: {
+    hello: () => {
+      return "Hello World";
     },
-  };
+
+    getListArticle: async () => {
+        const articles = await Article.find({
+            deleted : false
+        });
+        return articles;
+    },
+  },
+};
--- typeDefs.ts
+++ typeDefs.ts
@@ -2,7 +2,15 @@ import { gql } from "apollo-server-express";
 
 //Định nghĩa trường dữ liệu cho phép người ta lấy ra
 export const typeDefs = gql`
+  type Article {
+    id: ID
+    title: String
+    avatar: String
+    description: String
+  }
+
   type Query {
     hello: String
+    getListArticle: [Article]
   }
 `;
