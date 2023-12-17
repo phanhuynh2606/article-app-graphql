a055bbc552c352ebdb00aebc3975b34cc6033df8
Huynh Phan <huynhpvhe170150@fpt.edu.vn>
2023-12-17 11:15:51 +0700

    Kết nối Database

--- index.ts
+++ index.ts
@@ -1,12 +1,22 @@
 import express,{Express,Request,Response } from "express";
+import * as database from "./config/database";
+import dotenv from "dotenv";
+import Article from "./model/article.model";
+
+dotenv.config();
+
+database.connect();
 
 const app : Express = express();
-const port : number = 3000;
+const port : number | string = process.env.PORT || 3000;
 
 //Rest API  
-app.get("/articles",(req:Request , res : Response) =>{
+app.get("/articles", async(req:Request , res : Response) =>{
+    const articles = await Article.find({
+        deleted: false
+    });
     res.json({
-        articles : []
+        articles : articles
     });
 });
 
